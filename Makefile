f99c95d181062bc57c07d907be5461512a72a0b9
Pete Du <pete@jjj.work>
2020-12-08 12:51:56 +1100

    inital commit

---
+++ Makefile
@@ -0,0 +1,60 @@
+PACKER_BINARY ?= packer
+PACKER_VARIABLES := aws_region ami_name binary_bucket_name binary_bucket_region kubernetes_version kubernetes_build_date docker_version containerd_verion cni_plugin_version source_ami_id source_ami_owners arch instance_type security_group_id additional_yum_repos pull_cni_from_github
+
+K8S_VERSION_PARTS := $(subst ., ,$(kubernetes_version))
+K8S_VERSION_MINOR := $(word 1,${K8S_VERSION_PARTS}).$(word 2,${K8S_VERSION_PARTS})
+
+aws_region ?= $(AWS_DEFAULT_REGION)
+binary_bucket_region ?= $(AWS_DEFAULT_REGION)
+arch ?= x86_64
+ifeq ($(arch), arm64)
+instance_type ?= a1.large
+ami_name ?= amazon-eks-arm64-node-$(K8S_VERSION_MINOR)-v$(shell date +'%Y%m%d')
+else
+instance_type ?= m4.large
+ami_name ?= amazon-eks-node-$(K8S_VERSION_MINOR)-v$(shell date +'%Y%m%d')
+endif
+
+ifeq ($(aws_region), cn-northwest-1)
+source_ami_owners ?= 141808717104
+endif
+
+ifeq ($(aws_region), us-gov-west-1)
+source_ami_owners ?= 045324592363
+endif
+
+T_RED := \e[0;31m
+T_GREEN := \e[0;32m
+T_YELLOW := \e[0;33m
+T_RESET := \e[0m
+
+.PHONY: all
+all: 1.15 1.16 1.17 1.18
+
+.PHONY: validate
+validate:
+	$(PACKER_BINARY) validate $(foreach packerVar,$(PACKER_VARIABLES), $(if $($(packerVar)),--var $(packerVar)='$($(packerVar))',)) eks-worker-al2.json
+
+.PHONY: k8s
+k8s: validate
+	@echo "$(T_GREEN)Building AMI for version $(T_YELLOW)$(kubernetes_version)$(T_GREEN) on $(T_YELLOW)$(arch)$(T_RESET)"
+	$(PACKER_BINARY) build $(foreach packerVar,$(PACKER_VARIABLES), $(if $($(packerVar)),--var $(packerVar)='$($(packerVar))',)) eks-worker-al2.json
+
+# Build dates and versions taken from https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+
+
+.PHONY: 1.15
+1.15:
+	$(MAKE) k8s kubernetes_version=1.15.12 kubernetes_build_date=2020-11-02 pull_cni_from_github=true
+
+.PHONY: 1.16
+1.16:
+	$(MAKE) k8s kubernetes_version=1.16.15 kubernetes_build_date=2020-11-02 pull_cni_from_github=true
+
+.PHONY: 1.17
+1.17:
+	$(MAKE) k8s kubernetes_version=1.17.12 kubernetes_build_date=2020-11-02 pull_cni_from_github=true
+
+.PHONY: 1.18
+1.18:
+	$(MAKE) k8s kubernetes_version=1.18.9 kubernetes_build_date=2020-11-02 pull_cni_from_github=true
---
+++ scripts/install-worker.sh
@@ -0,0 +1,299 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+IFS=$'\n\t'
+
+TEMPLATE_DIR=${TEMPLATE_DIR:-/tmp/worker}
+
+################################################################################
+### Validate Required Arguments ################################################
+################################################################################
+validate_env_set() {
+    (
+        set +o nounset
+
+        if [ -z "${!1}" ]; then
+            echo "Packer variable '$1' was not set. Aborting"
+            exit 1
+        fi
+    )
+}
+
+validate_env_set BINARY_BUCKET_NAME
+validate_env_set BINARY_BUCKET_REGION
+validate_env_set DOCKER_VERSION
+validate_env_set CONTAINERD_VERSION
+validate_env_set CNI_PLUGIN_VERSION
+validate_env_set KUBERNETES_VERSION
+validate_env_set KUBERNETES_BUILD_DATE
+validate_env_set PULL_CNI_FROM_GITHUB
+
+################################################################################
+### Machine Architecture #######################################################
+################################################################################
+
+MACHINE=$(uname -m)
+if [ "$MACHINE" == "x86_64" ]; then
+    ARCH="amd64"
+elif [ "$MACHINE" == "aarch64" ]; then
+    ARCH="arm64"
+else
+    echo "Unknown machine architecture '$MACHINE'" >&2
+    exit 1
+fi
+
+################################################################################
+### Packages ###################################################################
+################################################################################
+
+# Update the OS to begin with to catch up to the latest packages.
+sudo yum update -y
+
+# Install necessary packages
+sudo yum install -y \
+    aws-cfn-bootstrap \
+    awscli \
+    chrony \
+    conntrack \
+    curl \
+    jq \
+    ec2-instance-connect \
+    nfs-utils \
+    socat \
+    unzip \
+    wget \
+    nfs-utils \
+    python3
+
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python3 get-pip.py
+rm get-pip.py
+
+sudo /usr/local/bin/pip3 install ansible
+
+
+# Remove the ec2-net-utils package, if it's installed. This package interferes with the route setup on the instance.
+if yum list installed | grep ec2-net-utils; then sudo yum remove ec2-net-utils -y -q; fi
+
+################################################################################
+### Time #######################################################################
+################################################################################
+
+# Make sure Amazon Time Sync Service starts on boot.
+sudo service chronyd restart
+sudo chkconfig chronyd on
+
+# Make sure that chronyd syncs RTC clock to the kernel.
+cat <<EOF | sudo tee -a /etc/chrony.conf
+# This directive enables kernel synchronisation (every 11 minutes) of the
+# real-time clock. Note that it can’t be used along with the 'rtcfile' directive.
+rtcsync
+EOF
+
+# If current clocksource is xen, switch to tsc
+if grep --quiet xen /sys/devices/system/clocksource/clocksource0/current_clocksource &&
+  grep --quiet tsc /sys/devices/system/clocksource/clocksource0/available_clocksource; then
+    echo "tsc" | sudo tee /sys/devices/system/clocksource/clocksource0/current_clocksource
+else
+    echo "tsc as a clock source is not applicable, skipping."
+fi
+
+################################################################################
+### iptables ###################################################################
+################################################################################
+
+# Enable forwarding via iptables
+sudo bash -c "/sbin/iptables-save > /etc/sysconfig/iptables"
+
+sudo mv $TEMPLATE_DIR/iptables-restore.service /etc/systemd/system/iptables-restore.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable iptables-restore
+
+################################################################################
+### Docker #####################################################################
+################################################################################
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+INSTALL_DOCKER="${INSTALL_DOCKER:-true}"
+if [[ "$INSTALL_DOCKER" == "true" ]]; then
+    sudo amazon-linux-extras enable docker
+    sudo groupadd -fog 1950 docker && sudo useradd --gid 1950 docker
+    sudo yum install -y docker-${DOCKER_VERSION}*
+    sudo usermod -aG docker $USER
+
+    # Remove all options from sysconfig docker.
+    sudo sed -i '/OPTIONS/d' /etc/sysconfig/docker
+
+    sudo mkdir -p /etc/docker
+    sudo mv $TEMPLATE_DIR/docker-daemon.json /etc/docker/daemon.json
+    sudo chown root:root /etc/docker/daemon.json
+
+    sudo yum downgrade -y containerd-${CONTAINERD_VERSION}
+
+    # Enable docker daemon to start on boot.
+    sudo systemctl daemon-reload
+    sudo systemctl enable docker
+fi
+
+################################################################################
+### Logrotate ##################################################################
+################################################################################
+
+# kubelet uses journald which has built-in rotation and capped size.
+# See man 5 journald.conf
+sudo mv $TEMPLATE_DIR/logrotate-kube-proxy /etc/logrotate.d/kube-proxy
+sudo mv $TEMPLATE_DIR/logrotate.conf /etc/logrotate.conf
+sudo chown root:root /etc/logrotate.d/kube-proxy
+sudo chown root:root /etc/logrotate.conf
+sudo mkdir -p /var/log/journal
+
+################################################################################
+### Kubernetes #################################################################
+################################################################################
+
+sudo mkdir -p /etc/kubernetes/manifests
+sudo mkdir -p /var/lib/kubernetes
+sudo mkdir -p /var/lib/kubelet
+sudo mkdir -p /opt/cni/bin
+
+echo "Downloading binaries from: s3://$BINARY_BUCKET_NAME"
+S3_DOMAIN="amazonaws.com"
+if [ "$BINARY_BUCKET_REGION" = "cn-north-1" ] || [ "$BINARY_BUCKET_REGION" = "cn-northwest-1" ]; then
+    S3_DOMAIN="amazonaws.com.cn"
+elif [ "$BINARY_BUCKET_REGION" = "us-iso-east-1" ]; then
+    S3_DOMAIN="c2s.ic.gov"
+elif [ "$BINARY_BUCKET_REGION" = "us-isob-east-1" ]; then
+    S3_DOMAIN="sc2s.sgov.gov"
+fi
+S3_URL_BASE="https://$BINARY_BUCKET_NAME.s3.$BINARY_BUCKET_REGION.$S3_DOMAIN/$KUBERNETES_VERSION/$KUBERNETES_BUILD_DATE/bin/linux/$ARCH"
+S3_PATH="s3://$BINARY_BUCKET_NAME/$KUBERNETES_VERSION/$KUBERNETES_BUILD_DATE/bin/linux/$ARCH"
+
+BINARIES=(
+    kubelet
+    aws-iam-authenticator
+)
+for binary in ${BINARIES[*]} ; do
+    if [[ -n "$AWS_ACCESS_KEY_ID" ]]; then
+        echo "AWS cli present - using it to copy binaries from s3."
+        aws s3 cp --region $BINARY_BUCKET_REGION $S3_PATH/$binary .
+        aws s3 cp --region $BINARY_BUCKET_REGION $S3_PATH/$binary.sha256 .
+    else
+        echo "AWS cli missing - using wget to fetch binaries from s3. Note: This won't work for private bucket."
+        sudo wget $S3_URL_BASE/$binary
+        sudo wget $S3_URL_BASE/$binary.sha256
+    fi
+    sudo sha256sum -c $binary.sha256
+    sudo chmod +x $binary
+    sudo mv $binary /usr/bin/
+done
+
+# Since CNI 0.7.0, all releases are done in the plugins repo.
+CNI_PLUGIN_FILENAME="cni-plugins-linux-${ARCH}-${CNI_PLUGIN_VERSION}"
+
+if [ "$PULL_CNI_FROM_GITHUB" = "true" ]; then
+    echo "Downloading CNI plugins from Github"
+    wget "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGIN_VERSION}/${CNI_PLUGIN_FILENAME}.tgz"
+    wget "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGIN_VERSION}/${CNI_PLUGIN_FILENAME}.tgz.sha512"
+    sudo sha512sum -c "${CNI_PLUGIN_FILENAME}.tgz.sha512"
+    rm "${CNI_PLUGIN_FILENAME}.tgz.sha512"
+else
+    if [[ -n "$AWS_ACCESS_KEY_ID" ]]; then
+        echo "AWS cli present - using it to copy binaries from s3."
+        aws s3 cp --region $BINARY_BUCKET_REGION $S3_PATH/${CNI_PLUGIN_FILENAME}.tgz .
+        aws s3 cp --region $BINARY_BUCKET_REGION $S3_PATH/${CNI_PLUGIN_FILENAME}.tgz.sha256 .
+        sudo sha256sum -c "${CNI_PLUGIN_FILENAME}.tgz.sha256"
+    else
+        echo "AWS cli missing - using wget to fetch cni binaries from s3. Note: This won't work for private bucket."
+        sudo wget "$S3_URL_BASE/${CNI_PLUGIN_FILENAME}.tgz"
+        sudo wget "$S3_URL_BASE/${CNI_PLUGIN_FILENAME}.tgz.sha256"
+    fi
+fi
+sudo tar -xvf "${CNI_PLUGIN_FILENAME}.tgz" -C /opt/cni/bin
+rm "${CNI_PLUGIN_FILENAME}.tgz"
+
+sudo rm ./*.sha256
+
+sudo mkdir -p /etc/kubernetes/kubelet
+sudo mkdir -p /etc/systemd/system/kubelet.service.d
+sudo mv $TEMPLATE_DIR/kubelet-kubeconfig /var/lib/kubelet/kubeconfig
+sudo chown root:root /var/lib/kubelet/kubeconfig
+sudo mv $TEMPLATE_DIR/kubelet.service /etc/systemd/system/kubelet.service
+sudo chown root:root /etc/systemd/system/kubelet.service
+sudo mv $TEMPLATE_DIR/kubelet-config.json /etc/kubernetes/kubelet/kubelet-config.json
+sudo chown root:root /etc/kubernetes/kubelet/kubelet-config.json
+
+
+sudo systemctl daemon-reload
+# Disable the kubelet until the proper dropins have been configured
+sudo systemctl disable kubelet
+
+################################################################################
+### EKS ########################################################################
+################################################################################
+
+sudo mkdir -p /etc/eks
+sudo mv $TEMPLATE_DIR/eni-max-pods.txt /etc/eks/eni-max-pods.txt
+sudo mv $TEMPLATE_DIR/bootstrap.sh /etc/eks/bootstrap.sh
+sudo chmod +x /etc/eks/bootstrap.sh
+
+################################################################################
+### AMI Metadata ###############################################################
+################################################################################
+
+BASE_AMI_ID=$(curl -s  http://169.254.169.254/latest/meta-data/ami-id)
+cat <<EOF > /tmp/release
+BASE_AMI_ID="$BASE_AMI_ID"
+BUILD_TIME="$(date)"
+BUILD_KERNEL="$(uname -r)"
+ARCH="$(uname -m)"
+EOF
+sudo mv /tmp/release /etc/eks/release
+sudo chown -R root:root /etc/eks
+
+################################################################################
+### Stuff required by "protectKernelDefaults=true" #############################
+################################################################################
+
+cat <<EOF | sudo tee -a /etc/sysctl.d/99-amazon.conf
+vm.overcommit_memory=1
+kernel.panic=10
+kernel.panic_on_oops=1
+EOF
+
+################################################################################
+### Cleanup ####################################################################
+################################################################################
+
+# Clean up yum caches to reduce the image size
+# Comment this section out and run it in the cleanup.sh provisioner step
+# sudo yum clean all
+# sudo rm -rf \
+#    $TEMPLATE_DIR  \
+#    /var/cache/yum
+
+
+# Clean up files to reduce confusion during debug
+#sudo rm -rf \
+#    /etc/hostname \
+#    /etc/machine-id \
+#    /etc/resolv.conf \
+#    /etc/ssh/ssh_host* \
+#    /home/ec2-user/.ssh/authorized_keys \
+#    /root/.ssh/authorized_keys \
+#    /var/lib/cloud/data \
+#    /var/lib/cloud/instance \
+#    /var/lib/cloud/instances \
+#    /var/lib/cloud/sem \
+#    /var/lib/dhclient/* \
+#    /var/lib/dhcp/dhclient.* \
+#    /var/lib/yum/history \
+#    /var/log/cloud-init-output.log \
+#    /var/log/cloud-init.log \
+#    /var/log/secure \
+#    /var/log/wtmp
+
+#sudo touch /etc/machine-id
